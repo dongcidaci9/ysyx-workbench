9e77a5b97f4b4d11156f73a508d0d629a1233245
tracer-ysyx <tracer@ysyx.org>
2024-01-09 00:06:59 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:06:59 up  5:04,  4 users,  load average: 1.00, 0.60, 0.48

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -62,13 +62,7 @@ static int cmd_si(char *args) {
 	return 0;
 }
 
-static int cmd_info(char *args) {
-	char *arg = strtok(NULL, " ");
-	if (arg == NULL) printf("ERROR: undefined command.\n");
-	if (strcmp(arg, "r") == 0) isa_reg_display();
-	return 0;
-}
-
+static int cmd_info(char *args);
 
 static int cmd_help(char *args);
 
@@ -80,7 +74,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "Program pauses execution after executing N instructions in a single step, when N is not given, the default is 1.", cmd_si},
+  { "si", "Program pauses execution after executing N instructions in a single step, when N is not given, the default is 1", cmd_si},
   { "info", "Print register status", cmd_info }
 
   /* TODO: Add more commands */
@@ -88,6 +82,13 @@ static struct {
 };
 
 #define NR_CMD ARRLEN(cmd_table)
+
+static int cmd_info(char *args) {
+	char *arg = strtok(NULL, " ");
+
+	if (strcmp(arg, "r") == 0) { isa_reg_display(); return 0; }
+	else { printf("ERROR: undefined command.\n"); return 0; }
+}
 
 static int cmd_help(char *args) {
   /* extract the first argument */
