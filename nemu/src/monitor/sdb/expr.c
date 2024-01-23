77528406b89e85743e8948aa3f392b91ce2c67b4
tracer-ysyx <tracer@ysyx.org>
2024-01-23 21:22:54 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:54 up  6:07,  3 users,  load average: 0.22, 0.33, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -127,7 +127,7 @@ static bool make_token(char *e) {
 
 bool check_parentheses(int p, int q) {
 	int par = 0;
-	if (tokens[p].type == ')' && tokens[q].type == ')') {
+	if (tokens[p].type == ')' || tokens[q].type == ')') {
 		for (int i = p; i <= q; i ++) {
 			if (tokens[i].type == '(') par ++;
 			else if (tokens[i].type == ')') par --;
