ce4dc1c793930a97cc64344f1879c8588eaedc0b
tracer-ysyx <tracer@ysyx.org>
2024-01-23 15:14:29 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:14:29 up  4:28,  3 users,  load average: 0.25, 0.18, 0.17

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -110,7 +110,6 @@ static bool make_token(char *e) {
 					case TK_NUM: case TK_HEXNUM:
 						strncpy(tokens[nr_token].str, substr_start, substr_len);
 						tokens[nr_token].str[substr_len] = '\0';
-					default: continue;
 					}
 				nr_token ++;
 				break;
