5cd61d48b5aef5e77fd557f49a9a7d444c940d8e
tracer-ysyx <tracer@ysyx.org>
2024-01-23 21:28:43 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:43 up  6:12,  3 users,  load average: 0.51, 0.50, 0.39

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -132,7 +132,7 @@ bool check_parentheses(int p, int q) {
 			if (tokens[i].type == '(') par ++;
 			else if (tokens[i].type == ')') par --;
 			
-			if (par == 0) return true; 
+			if (par == 0) return (i==q); 
 		}
 	} 
 	return false;
