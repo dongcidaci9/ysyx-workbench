b5443fe3fbf7a109fae3f7b3b767223dc387da75
tracer-ysyx <tracer@ysyx.org>
2024-01-25 18:59:44 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:59:44 up  1:08,  3 users,  load average: 0.21, 0.18, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -230,20 +230,19 @@ void test_expr() {
   word_t correct_res;
   size_t len = 0;
   ssize_t read;
-  bool *success = false;
+  bool success = false;
 
   while (true) {
-    if(fscanf(fp, "%u ", &correct_res) == -1) break;
+    if (fscanf(fp, "%u ", &correct_res) == -1) break;
     read = getline(&e, &len, fp);
-    e[read] = '\0';
+    if (e[read-1] == '\n') e[read-1] = '\0';
     
-    word_t res = expr(e, success);
+    word_t result = expr(e, &success);
     assert(success);
 
-    if (res != correct_res) {
+    if (result == correct_res) {
       puts(e);
-      printf("expected: %u, got: %u\n", correct_res, res);
-      assert(0);
+      printf("expected: %u, got: %u\n", correct_res, result);
     }
   }
 
