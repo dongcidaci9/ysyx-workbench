ab52334cc76dc437533eb0a733cbe64756b1b937
tracer-ysyx <tracer@ysyx.org>
2024-01-26 14:43:36 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:43:36 up  3:22,  8 users,  load average: 0.42, 0.37, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -103,7 +103,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-				if (rules[i].token_type == TK_NOTYPE) break;
+				if (rules[i].token_type == TK_NOTYPE) continue;
 
 				tokens[nr_token].type = rules[i].token_type;
 
