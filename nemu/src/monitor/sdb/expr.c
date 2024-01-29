94f864fa2560d686504c53a7b91afce5b3293b63
tracer-ysyx <tracer@ysyx.org>
2024-01-29 14:48:15 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:48:15 up  2:52,  5 users,  load average: 0.41, 0.37, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -217,16 +217,19 @@ static word_t operand(int i, bool *success) {
 // Calculate normal affair:
 static word_t calc1(word_t val1, int operator, word_t val2, bool *success) {
 	switch(operator) {
-      case '+': return val1 + val2;
-      case '-': return val1 - val2;
-      case '*': return val1 * val2;
-      case '/': if (val2 == 0) {
-									*success = false;
-									return 0;
-								} else {
-									return (sword_t)val1 / (sword_t)val2;
-								}
-			default: assert(0);
+		case TK_EQ: return val1 == val2;
+		case TK_NEQ: return val1 != val2;
+    case '+': return val1 + val2;
+    case '-': return val1 - val2;
+    case '*': return val1 * val2;
+    case '/': 
+			if (val2 == 0) {
+				*success = false;
+				return 0;
+			} else {
+				return (sword_t)val1 / (sword_t)val2;
+			}
+		default: assert(0);
 	}
 	return 0;
 }
