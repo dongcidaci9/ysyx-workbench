0ddff8175fde6e7f2286a693fa53ca20c398ee79
tracer-ysyx <tracer@ysyx.org>
2024-01-29 12:07:29 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:07:29 up  2:27,  5 users,  load average: 0.17, 0.16, 0.17

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -104,9 +104,9 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "q", "Exit NEMU", cmd_q },
   { "si", "Program pauses execution after executing N instructions in a single step, when N is not given, the default is 1", cmd_si },
-  { "info", "press \"r\" can print registers status/ press \"w\" can print watchpoints status", cmd_info },
+  { "info", "Press \"r\" can print registers status/ Press \"w\" can print watchpoints status", cmd_info },
   { "x", "As starting memory address, output N consecutive 4 bytes in hexadecimal form", cmd_x },
-  { "p", "Find the value of the expression EXPR/ press \"test\" can do the testing", cmd_p }
+  { "p", "Find the value of the expression EXPR/ Press \"test\" can do the testing", cmd_p }
 
   /* TODO: Add more commands */
 
