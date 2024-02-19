375c66eee0b83df66b2ca4ce7a45bfe6d0e15d20
tracer-ysyx <tracer@ysyx.org>
2024-02-20 00:53:33 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:53:33 up  2:34,  3 users,  load average: 0.79, 0.59, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -244,7 +244,7 @@ static word_t calc2(int operator, word_t val, bool *success) {
     switch (operator) {
 			case TK_NEG: return -val; 
 			case TK_POS: return val;
-			case TK_DEREF : return (val == TK_PC ? nemu_state.halt_pc: (uintptr_t)&val);
+			case TK_DEREF : return (val == TK_PC ? 100: (uintptr_t)&val);
 			default: assert(0);
 		}
 		return 0;
