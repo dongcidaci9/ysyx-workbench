c2c4e327bd26a14f00ac1bf2e3284d16cbd65c08
tracer-ysyx <tracer@ysyx.org>
2024-03-25 10:40:07 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:40:07 up  1:06,  3 users,  load average: 0.50, 0.45, 0.42

--- npc/vsrc/ysyx_23060201_EXU.v
+++ npc/vsrc/ysyx_23060201_EXU.v
@@ -13,7 +13,11 @@ module ysyx_23060201_EXU(
 		.a(inst_rd_val_in),
 		.b(inst_imm),
 		.ctrl(ctrl),
-		.res(inst_rd_val_out)
+		.res(inst_rd_val_out),
+		.nf()),
+		.zf()),
+		.cf()),
+		.vf())
 	);
 
 endmodule
