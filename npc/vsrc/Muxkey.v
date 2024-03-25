3f43b1bd0dc8d4b69ec3fa92a4ac95c418c0905a
tracer-ysyx <tracer@ysyx.org>
2024-03-25 10:32:15 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:32:15 up 58 min,  3 users,  load average: 0.77, 0.55, 0.42

--- npc/vsrc/Muxkey.v
+++ npc/vsrc/Muxkey.v
@@ -42,13 +42,3 @@ module MuxKey #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
 );
   MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 0) i0 (out, key, {DATA_LEN{1'b0}}, lut);
 endmodule
-
-// 带默认值的选择器模板
-module MuxKeyWithDefault #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
-  output [DATA_LEN-1:0] out,
-  input [KEY_LEN-1:0] key,
-  input [DATA_LEN-1:0] default_out,
-  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
-);
-  MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 1) i0 (out, key, default_out, lut);
-endmodule
