8e1998837ccd35f9caf731f8b8efdea6218bf69d
tracer-ysyx <tracer@ysyx.org>
2024-03-27 13:36:31 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:36:31 up  3:07,  4 users,  load average: 0.35, 0.23, 0.19

--- npc/vsrc/ysyx_23060201_IDU.v
+++ npc/vsrc/ysyx_23060201_IDU.v
@@ -1,28 +1,21 @@
 module ysyx_23060201_IDU(
 	input [31:0] inst,
-	output [9:0] inst_type, 
+	output [6:0] inst_op,
+	output [2:0] inst_func3,
 	output [4:0] inst_rs1, inst_rd,
 	output [31:0] inst_imm // RV64
 ); // inst -> type_judge -> inst_split_type(op, func3) -> rs1, rs2, rd, imm
 
 	// type_judge // addi
-	wire [6:0] op;
-	wire [2:0] func3;
 	wire [4:0] rs1, rd;
 	wire [11:0] imm;
 
 	// inst_split_type
-	assign op = inst[6:0];
-	assign func3 = inst[14:12];
-	assign inst_type = {func3, op};
-
-	// rs1, rs2, rd, imm
-	assign rs1 = inst[19:15];
-	assign rd = inst[11:7];
+	assign inst_op = inst[6:0];
+	assign inst_func3 = inst[14:12];
+	assign inst_rs1 = inst[19:15];
+	assign inst_rd = inst[11:7];
 	assign imm = inst[31:20];
-
-	assign inst_rs1 = rs1;
-	assign inst_rd = rd;
 
 	// sign extension
 	wire [31:0] imm_ext;
