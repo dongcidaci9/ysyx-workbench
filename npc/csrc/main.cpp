7ff3ef9792401ebf532510acda9176aabff0ad74
tracer-ysyx <tracer@ysyx.org>
2024-04-01 15:15:23 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:23 up  3:47,  7 users,  load average: 0.75, 0.60, 0.50

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -9,8 +9,8 @@ VerilatedVcdC* vcd = NULL;
 
 static void step_and_dump_wave(){
 	top->eval();
-	vcd->dump(contextp->time());
 	contextp->timeInc(1);
+	vcd->dump(contextp->time());
 }
 
 // Initialize
@@ -19,7 +19,7 @@ static void sim_init(){
 	vcd = new VerilatedVcdC;
 	top = new Vysyx_23060201_TOP;
 	contextp->traceEverOn(true);
-	top->trace(vcd, 99);
+	top->trace(vcd, 0);
 	vcd->open("wave.vcd");
 }
 
@@ -33,22 +33,23 @@ int main() {
 	sim_init();
 
 	top->rst = 1;
+	step_and_dump_wave();
 	top->rst = 0;
 
 	top->inst = 0b00000000000100000000000010010011; // x1 = x0 + 1
-	step_and_dump_wave;
+	step_and_dump_wave();
 	printf("pc = %d, inst_rd_val_res = %d\n", top->pc ,top->inst_rd_val_res);
 
 	top->inst = 0b00000000001000000000000010010011; // x1 = x0 + 2
-	step_and_dump_wave;
+	step_and_dump_wave();
 	printf("pc = %d, inst_rd_val_res = %d\n", top->pc ,top->inst_rd_val_res);
 
 	top->inst = 0b00000000000100001000000100010011; // x2 = x1 + 1
-	step_and_dump_wave;
+	step_and_dump_wave();
 	printf("pc = %d, inst_rd_val_res = %d\n", top->pc ,top->inst_rd_val_res);
 
 	top->inst = 0b00000000001100010000000100010011; // x2 = x2 + 3
-	step_and_dump_wave;
+	step_and_dump_wave();
 	printf("pc = %d, inst_rd_val_res = %d\n", top->pc ,top->inst_rd_val_res);
 
 	sim_exit();
