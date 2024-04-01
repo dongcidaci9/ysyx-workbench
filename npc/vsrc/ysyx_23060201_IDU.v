9fc4b5d0eabab3982a2853d44030fae3f6493b28
tracer-ysyx <tracer@ysyx.org>
2024-04-01 15:11:18 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:11:18 up  3:43,  7 users,  load average: 0.33, 0.36, 0.42

--- npc/vsrc/ysyx_23060201_IDU.v
+++ npc/vsrc/ysyx_23060201_IDU.v
@@ -6,7 +6,6 @@ module ysyx_23060201_IDU(
 	output [31:0] inst_imm // RV64
 ); // inst -> type_judge -> inst_split_type(op, func3) -> rs1, rs2, rd, imm
 
-
 	// inst_split_type
 	wire [11:0] imm;
 
@@ -22,12 +21,14 @@ module ysyx_23060201_IDU(
 	assign inst_imm = imm_ext;
 
 	// DPI-C
+	/*
 	import "DPI-C" function void npc_trap();
 	always @(*) begin
 		if (inst_op == 7'b1110011 && inst_func3 == 3'b000) begin
 			npc_trap();
 		end
 	end
+	*/
 	
 endmodule
 
