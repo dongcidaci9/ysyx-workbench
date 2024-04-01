e84c7f59b5f307ccfedbeaaa2049b4c4dd7ee131
tracer-ysyx <tracer@ysyx.org>
2024-04-01 12:48:03 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:48:03 up  2:29,  5 users,  load average: 0.18, 0.14, 0.07

--- npc/csrc/ysyx_23060201_TOP.cpp
+++ npc/csrc/ysyx_23060201_TOP.cpp
@@ -1,50 +1,51 @@
-#include <stdlib.h>
-#include <stdint.h>
-
 #include "Vysyx_23060201_TOP.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include <stdlib.h>
+#include <stdint.h>
+
 
 uint32_t *init_mem(size_t size);
 uint32_t guest_to_host(uint32_t addr);
 uint32_t pmem_read(uint32_t *memory, uint32_t vaddr);
 
+// 
 VerilatedContext* contextp = NULL;
-VerilatedVcdC* tfp = NULL;
+VerilatedVcdC* vcd = NULL;
 
-static Vysyx_23060201_TOP top;
+static Vysyx_23060201_TOP* top;
 
 static void step_and_dump_wave(){
 	top->eval();
+	vcd->dump(contextp->time());
 	contextp->timeInc(1);
-	tfp->dump(contextp->time());
 }
 
 // Initialize
 static void sim_init(){
 	contextp = new VerilatedContext;
-	tfp = new VerilatedVcdC;
+	vcd = new VerilatedVcdC;
 	top = new Vysyx_23060201_TOP;
 	contextp->traceEverOn(true);
-	top->trace(tfp, 0);
+	top->trace(vcd, 99);
 	tfp->open("wave.vcd");
 }
 
 // Exit
 static void sim_exit(){
 	step_and_dump_wave();
-	tfp->close();
+	vcd->close();
 }
 
 static void single_cycle() {
-	dut.clk = 0; dut.eval();
-	dut.clk = 1; dut.eval();
+	top.clk = 0; top.eval();
+	top.clk = 1; top.eval();
 }
 
 static void rst(int n) {
-	dut.rst = 1;
+	top.rst = 1;
 	while(n-->0) single_cycle();
-	dut.rst = 0;
+	top.rst = 0;
 }
 
 int main() {
