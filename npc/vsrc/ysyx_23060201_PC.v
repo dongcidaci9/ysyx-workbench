12fe7002946e8dff66237c375ed8c1bfa0f7f491
tracer-ysyx <tracer@ysyx.org>
2024-04-01 16:44:02 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:44:02 up  5:16,  7 users,  load average: 0.24, 0.33, 0.35

--- npc/vsrc/ysyx_23060201_PC.v
+++ npc/vsrc/ysyx_23060201_PC.v
@@ -6,7 +6,7 @@ module ysyx_23060201_PC(
 
 	wire [31:0] add_in, npc; // intern signal
 
-	Reg #(32, 32'h80000000) pc_reg (clk, rst, npc, pc, 1'b1); // next pc
+	Reg #(32, 32'h10000000) pc_reg (clk, rst, npc, pc, 1'b1); // next pc
 	
 	assign add_in = pc;
 	assign npc = add_in + 32'h4; // pc + 4
