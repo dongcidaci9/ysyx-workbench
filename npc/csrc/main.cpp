eca3cb2052e2ec669e4d6ad92d735305b224e023
tracer-ysyx <tracer@ysyx.org>
2024-04-02 15:12:42 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:12:42 up  1:35,  4 users,  load average: 0.71, 0.47, 0.34

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -68,8 +68,7 @@ int main() {
 	top->clk = 0; step_and_dump_wave();
 	printf("pc = 0x%x, inst_rd_val = %d\n", top->pc ,top->inst_rd_val);
 	// rd = 6
-	top->clk = 1; step_and_dump_wave();
-	top->inst = 0b00000000000100010000000100010011; // addi x2 = x2 + 1
+	top->clk = 1; step_and_dump_wave(); top->inst = 0b00000000000100010000000100010011; // addi x2 = x2 + 1
 	top->clk = 0; step_and_dump_wave();
 	printf("pc = 0x%x, inst_rd_val = %d\n", top->pc ,top->inst_rd_val);
 	// rd = 7
@@ -79,7 +78,9 @@ int main() {
 
 // DPI-C
 extern "C" void npc_trap(){
+	
 	printf("<ebreak>\n");
+	sim_exit();
 	exit(0);
 }
 
