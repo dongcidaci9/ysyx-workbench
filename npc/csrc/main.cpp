c73ae79e8f11432705c09624a9f9e9fb1ddfc5b6
tracer-ysyx <tracer@ysyx.org>
2024-04-02 12:55:27 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:55:27 up 56 min,  3 users,  load average: 0.27, 0.42, 0.32

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -28,6 +28,7 @@ static void sim_init(){
 static void sim_exit(){
 	step_and_dump_wave();
 	vcd->close();
+	printf("Simulation finished. Total simulation time: %lld\n", contextp->time());
 }
 
 int main() {
@@ -35,32 +36,39 @@ int main() {
 
 	top->clk = 1; step_and_dump_wave();
 	top->clk = 0; step_and_dump_wave();
+
 	top->clk = 1; top->rst = 1; step_and_dump_wave();
 	printf("pc = 0x%x, inst_rd_val = %d\n", top->pc ,top->inst_rd_val);
 	top->clk = 0; top->rst = 0; step_and_dump_wave();
 	// rd = 0
 	top->clk = 1; step_and_dump_wave();
-	top->inst = 0b00000000000100000000000010010011; // x1 = x0 + 1
+	top->inst = 0b00000000000100000000000010010011; // addi x1 = x0 + 1
 	top->clk = 0; step_and_dump_wave();
 	printf("pc = 0x%x, inst_rd_val = %d\n", top->pc ,top->inst_rd_val);
 	// rd = 1
 	top->clk = 1; step_and_dump_wave();
-	top->inst = 0b00000000001000000000000010010011; // x1 = x0 + 2
+	top->inst = 0b00000000001000000000000010010011; // addi x1 = x0 + 2
 	top->clk = 0; step_and_dump_wave();
 	printf("pc = 0x%x, inst_rd_val = %d\n", top->pc ,top->inst_rd_val);
 	// rd = 2
 	top->clk = 1; step_and_dump_wave();
-	top->inst = 0b00000000000100001000000100010011; // x2 = x1 + 1
+	top->inst = 0b00000000000100001000000100010011; // addi x2 = x1 + 1
 	top->clk = 0; step_and_dump_wave();
 	printf("pc = 0x%x, inst_rd_val = %d\n", top->pc ,top->inst_rd_val);
 	// rd = 3
+
 	top->clk = 1; step_and_dump_wave();
-	top->inst = 0b00000000001100010000000100010011; // x2 = x2 + 3
+	top->inst = 0b00000000000100000000000001110011; // ebreak
+	top->clk = 0; step_and_dump_wave();
+
+	// ebreak
+	top->clk = 1; step_and_dump_wave();
+	top->inst = 0b00000000001100010000000100010011; // addi x2 = x2 + 3
 	top->clk = 0; step_and_dump_wave();
 	printf("pc = 0x%x, inst_rd_val = %d\n", top->pc ,top->inst_rd_val);
 	// rd = 6
 	top->clk = 1; step_and_dump_wave();
-	top->inst = 0b00000000000100010000000100010011; // x2 = x2 + 1
+	top->inst = 0b00000000000100010000000100010011; // addi x2 = x2 + 1
 	top->clk = 0; step_and_dump_wave();
 	printf("pc = 0x%x, inst_rd_val = %d\n", top->pc ,top->inst_rd_val);
 	// rd = 7
