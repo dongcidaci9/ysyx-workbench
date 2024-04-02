3c715daf4eaa6603ac4089ed1ccd6bd1cae2b797
tracer-ysyx <tracer@ysyx.org>
2024-04-02 11:38:38 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:38:38 up 23 min,  4 users,  load average: 0.49, 0.53, 0.42

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -32,17 +32,16 @@ static void sim_exit(){
 int main() {
 	sim_init();
 
-	top->clk = 1; top->rst = 1;
-	step_and_dump_wave();
+
+	top->clk = 1; step_and_dump_wave();
+	top->clk = 0; step_and_dump_wave();
+	top->clk = 1; top->rst = 1; step_and_dump_wave();
 	printf("pc = %d, inst_rd_val_res = %d\n", top->pc ,top->inst_rd_val);
-	top->clk = 0; top->rst = 0;
-	step_and_dump_wave();
+	top->clk = 0; top->rst = 0; step_and_dump_wave();
 	// rd = 0
-	top->clk = 1;
-	step_and_dump_wave();
+	top->clk = 1; step_and_dump_wave();
 	top->inst = 0b00000000000100000000000010010011; // x1 = x0 + 1
-	top->clk = 0;
-	step_and_dump_wave();
+	top->clk = 0; step_and_dump_wave();
 	printf("pc = %d, inst_rd_val_res = %d\n", top->pc ,top->inst_rd_val);
 	// rd = 1
 	top->clk = 1;
