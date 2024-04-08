bafb8ad95c42644b482347f4f3697e9ce50759a3
tracer-ysyx <tracer@ysyx.org>
2024-04-08 23:27:21 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:27:21 up 19 min,  3 users,  load average: 0.85, 0.55, 0.39

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -100,9 +100,9 @@ void wp_iterate() {
 	int i = 0;
 	while (Node_) {
 		i ++;
-		printf("\33[0;32mWP%02d: %s\33[0m\t", Node_->NO, Node_->expr);
+		printf("\33[0;32mWP%02d: %-10s\33[0m\t", Node_->NO, Node_->expr);
 		Node_ = Node_->next;
-		if (i % 3 == 0) printf("\n");
+		if (i % 2 == 0) printf("\n");
 	} 
 	printf("\n");
 }
