b4895bf5d4ba3f7514c14fea5fb7df3531b9c0e0
tracer-ysyx <tracer@ysyx.org>
2024-05-17 10:48:50 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:50 up  8:32,  4 users,  load average: 0.70, 0.44, 0.45

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,26 +13,35 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-static int intcpy(char *s, int num) {
-	int divisor = 100;
-	
-	char *ptr = s;
+static void itoa(unsigned int n, char *buf) {
 	int i = 0;
+	if (n < 10) {
+		buf[0] = '0' + n;
+		buf[1] = '\0';
+		return;
+	}
+
+	itoa(n / 10, buf);
+
+	for(i = 0; buf[i] != '\0'; i++);
+	buf[i] = (n % 10) + '0';
+	buf[i + 1] = '\0';
+}
+/*
 	while (divisor > 0) {
 		int digit = num / divisor;
 		if (digit != 0 || divisor == 1 ) ptr[i ++] = '0' + digit;
 		num %= divisor;
 		divisor /= 10;
 	}
-
-	return i;
 }
+*/
 
 int sprintf(char *out, const char *fmt, ...) {	
 	char *start = out; // record number of written character
 
-	va_list pArgs; // pointor towards variable parameter list
-	va_start(pArgs, fmt); // initialize it towards fmt, no matter what variable parameter type it is
+	va_list ap; // pointor towards variable parameter list
+	va_start(ap, fmt); // initialize it towards fmt, no matter what variable parameter type it is
 
 	while (*fmt != '\0') {
 		if (*fmt != '%') {
@@ -41,11 +50,11 @@ int sprintf(char *out, const char *fmt, ...) {
 			++ fmt;
 			switch (*fmt) {
 				case 'd':
-					int num = va_arg(pArgs, int);
-					out += intcpy(out, num);
+					unsigned int num = va_arg(ap, unsigned int);
+					itoa(num, out);
 					break;
 				case 's': 
-					char *s = va_arg(pArgs, char *);
+					char *s = va_arg(ap, char *);
 					strcpy(out, s);
 					out += strlen(out);
 					break;
@@ -55,9 +64,9 @@ int sprintf(char *out, const char *fmt, ...) {
 	}
 
 	*out = '\0';
-	va_end(pArgs); // finisth visit va_list
+	va_end(ap); // finisth visit va_list
 
-	return out - start + 1; 
+	return out - start; 
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
