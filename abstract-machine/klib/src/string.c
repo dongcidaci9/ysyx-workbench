3b30ced2541a9ab9f6e569636cfea8809d9f6548
tracer-ysyx <tracer@ysyx.org>
2024-05-22 20:28:08 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:28:08 up 12:47,  7 users,  load average: 0.10, 0.23, 0.36

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -66,7 +66,21 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+	unsigned char *d = dst;
+	const unsigned char *s = src;
+
+	if (d + n < s || d >= s + n) {
+		while (n --) {
+			*d ++ = *s ++;
+		}
+	} else {
+		d += n; // dst_end
+		s += n; // src_end
+		while (n -- ) {
+			*(-- d) = *(-- s);
+		}
+	}
+	return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
