45380ec7e4a17b7000618dfdb63cf63f2ee4869c
tracer-ysyx <tracer@ysyx.org>
2024-05-26 23:46:21 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:46:21 up 27 min,  6 users,  load average: 0.84, 0.84, 0.48

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -36,12 +36,11 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-	addr = (void *)ROUNDUP(heap.start, 8);
-	size = (size_t)ROUNDUP(size, 8);
+	addr = (void *)ROUNDUP(heap.start, 4);
+	size = (size_t)ROUNDUP(size, 4);
 
 	char *old = addr;
 	addr += size;
-	heap.start += size;
 	assert((uintptr_t)heap.start <= (uintptr_t) addr && (uintptr_t) addr < (uintptr_t)heap.end);
 	for (uint64_t *p = (uint64_t *)old; p !=(uint64_t *)addr; p ++) {
 		*p = 0;
