e57182215b8cf8a0334d95e7b5c7ec3f688661b2
tracer-ysyx <tracer@ysyx.org>
2024-05-28 00:22:46 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:22:46 up  6:24,  9 users,  load average: 0.58, 0.58, 0.58

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -36,7 +36,7 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-  #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
 	if (!init_flag) {
 		addr = (void *)ROUNDUP(heap.start, 8);
 		init_flag = true;
@@ -46,8 +46,8 @@ void *malloc(size_t size) {
 	addr += size;
 
 	return old;
-
 #endif
+	return NULL;
 }
 
 void free(void *ptr) {
