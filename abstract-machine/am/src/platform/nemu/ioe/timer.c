9632c5ec4e980ce7cbcd2212e143b47a10393eb6
tracer-ysyx <tracer@ysyx.org>
2024-05-28 20:18:15 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:18:15 up 59 min,  9 users,  load average: 0.14, 0.17, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -8,7 +8,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = inl(RTC_ADDR + 4);
-  uptime->us <<= 32;
+  uptime->us <<= 32; // 4 * 8
   uptime->us += inl(RTC_ADDR);
 }
 
