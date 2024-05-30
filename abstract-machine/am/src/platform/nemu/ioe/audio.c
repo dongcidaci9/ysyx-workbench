f03b552e39d975fa0b05f8af2325f95a17ae21a2
tracer-ysyx <tracer@ysyx.org>
2024-05-30 20:16:51 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:51 up  2:35,  6 users,  load average: 1.41, 1.04, 0.70

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -35,7 +35,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
 	uint32_t len = (ctl->buf).end - (ctl->buf).start;
 	// wait until have enough space
 	uint32_t used_cnt = inl(AUDIO_COUNT_ADDR);
-	while (len > sbuf_size - used_cnt);
+	while (len > sbuf_size - used_cnt) break;
 	
 	uint8_t *asb = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;
 		for (int i = 0; i < len; i ++) {
