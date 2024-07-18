8059b9e18a8f3195ed57ed468092a8506f077bcc
tracer-ysyx <tracer@ysyx.org>
2024-07-18 22:17:45 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:17:45 up  4:14,  3 users,  load average: 0.34, 0.35, 0.26

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -25,6 +25,7 @@ static uint8_t *pmem = NULL;
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
+// convert physics address of guest to virtual address of host
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
