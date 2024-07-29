e6202845ae62029a456a831af13b6f0b976913a8
tracer-ysyx <tracer@ysyx.org>
2024-07-29 17:33:02 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:33:02 up 20 min,  1 user,  load average: 0.97, 0.85, 0.59

---
+++ npc/include/c/common.h
@@ -0,0 +1,14 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdint.h>
+// some defines
+typedef uint32_t word_t;
+typedef uint32_t addr_t; 
+
+#define MBASE  0x80000000
+#define MSIZE  0x8000000
+#define	MLEFT  (addr_t)MBASE
+#define	MRIGHT (addr_t)MBASE + MSIZE - 1;
+
+#endif
