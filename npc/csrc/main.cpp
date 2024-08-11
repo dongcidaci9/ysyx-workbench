232e66cd1fce9a38c9aa6324ce3085f53d73610e
tracer-ysyx <tracer@ysyx.org>
2024-08-11 22:15:36 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:15:36 up  4:33,  3 users,  load average: 0.24, 0.26, 0.23

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -148,9 +148,9 @@ int main(int argc, char *argv[]) {
 	uint64_t n = 20;
 	for (;n > 0; n --) {
 		uint32_t pc = top->pc;
-		top->clk = 0; step_and_dump_wave();
 		top->inst = inst_fetch(&pc);
 		printf("pc: %#x, inst: %#010x\n", top->pc, top->inst);
+		top->clk = 0; step_and_dump_wave();
 		top->clk = 1; step_and_dump_wave();
 	}
 	// ebreak
