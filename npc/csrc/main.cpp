78244ec52fa6faaefd7058ec7b6c410b42c295ab
tracer-ysyx <tracer@ysyx.org>
2024-08-11 20:06:17 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:06:17 up  2:23,  3 users,  load average: 0.51, 0.31, 0.21

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -143,7 +143,9 @@ int main(int argc, char *argv[]) {
 	printf("(start)\n");
 	printf("pc: %#x\n", top->pc);
 
-	top->rst = 0; 
+	top->clk = 0; step_and_dump_wave();
+	top->rst = 1; // reset
+	top->clk = 1; step_and_dump_wave();
 	printf("(NPC running)\n");
 	uint64_t n = 11;
 	for (;n > 0; n --) {
