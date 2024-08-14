9c406b6cf5cd195cc04fe33ceca9c5cb80791729
tracer-ysyx <tracer@ysyx.org>
2024-08-14 22:33:13 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:33:13 up  1:06,  3 users,  load average: 0.44, 0.63, 0.69

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -145,6 +145,7 @@ static void execute(uint64_t n) {
 
 void cpu_exec(uint64_t n) {
 	g_print_step = (n < MAX_INST_TO_PRINT);
+	IFDEF(CONFIG_ITRACE, puts("aa"));
 	switch (npc_state.state) {
 		case NPC_END: case NPC_ABORT:
 			printf("Program execution has ended. To restart the program, exit NPC and run again.\n");
