98ffa631ff0faaedd2369c3c92757f991c468032
tracer-ysyx <tracer@ysyx.org>
2024-08-15 11:39:38 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:39:38 up  4:30,  3 users,  load average: 0.99, 0.64, 0.61

--- npc/csrc/utils/filelist.mk
+++ npc/csrc/utils/filelist.mk
@@ -1,4 +1,4 @@
 SIM_SRC += csrc/utils/disasm.cc
-COMPILE += -CFLAGS "$(shell llvm-config --cxxflags)"
+#COMPILE += -CFLAGS "$(shell llvm-config --cxxflags)"
 COMPILE += -CFLAGS "-fvisibility=hidden"
 LIBS += -LDFLAGS "$(shell llvm-config --libs)"
