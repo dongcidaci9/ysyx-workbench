99460fa0e129ecfa54e587a672ac74ddb9f88ea8
tracer-ysyx <tracer@ysyx.org>
2024-08-20 21:17:47 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:47 up  7:41,  3 users,  load average: 0.63, 0.53, 0.49

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -8,12 +8,13 @@ AM_SRCS := riscv/npc/start.S \
            platform/dummy/vme.c \
            platform/dummy/mpe.c
 
-CFLAGS      += -fdata-sections -ffunction-sections
-LDFLAGS     += -T $(AM_HOME)/scripts/linker.ld \
-						 --d    efsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
-LDFLAGS     += --gc-sections -e _start
-NPCFLAGS    += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
-NPCFLAGS    += -e $(IMAGE).elf
+CFLAGS    += -fdata-sections -ffunction-sections
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
+						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --gc-sections -e _start
+NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+NPCFLAGS += -e $(IMAGE).elf
+NPCFLAGS += -b
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
@@ -24,7 +25,4 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NPC_HOME) run ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
-
-gdb: image
-	$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
