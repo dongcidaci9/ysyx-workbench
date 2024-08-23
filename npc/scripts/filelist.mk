e8854acceddbff290f1ca5255d2fd200eb2b6263
tracer-ysyx <tracer@ysyx.org>
2024-08-23 14:40:38 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:38 up  3:56,  1 user,  load average: 0.04, 0.12, 0.23

--- npc/scripts/filelist.mk
+++ npc/scripts/filelist.mk
@@ -1,11 +1,11 @@
 ifeq ($(CONFIG_ITRACE), y)
-SIM_SRC += csrc/utils/disasm.cc
+SIM_SRC += csrc/utils/disasm.ccccc
 #SIM_SRC += csrc/utils/itrace.c
 LIBS += -LDFLAGS "$(shell llvm-config --libs)"
 endif
 
 ifeq ($(CONFIG_FTRACE), y)
-SIM_SRC += csrc/utils/ftrace.ccc
+SIM_SRC += csrc/utils/ftrace.cc
 endif
 
 ifeq ($(CONFIG_DIFFTEST), y)
