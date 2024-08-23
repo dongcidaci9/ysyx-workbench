c95e12e37530f685311dced8759e6934ad1e5d1c
tracer-ysyx <tracer@ysyx.org>
2024-08-23 17:33:25 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:33:25 up  6:49,  3 users,  load average: 1.15, 0.66, 0.45

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -37,7 +37,7 @@ static void restart() {
 void init_isa() {
   /* Load built-in image. */
   memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
-	printf("aaa\n");
+  printf("0x%08x\n", RESET_VECTOR);
 
   /* Initialize this virtual computer system. */
   restart();
