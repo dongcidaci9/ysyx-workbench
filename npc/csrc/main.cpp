d4e986933332257b35fe95aeb02afa826078011a
tracer-ysyx <tracer@ysyx.org>
2024-08-23 14:11:43 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:11:43 up  3:27,  1 user,  load average: 0.31, 0.34, 0.33

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -157,6 +157,11 @@ static void func_trace(Decode *s)
 #endif
 
 static void exec_once(Decode *s) {
+	top->inst = inst_fetch(&s->pc);
+	
+	top->clk = 0; step_and_dump_wave();
+	top->clk = 1; step_and_dump_wave();
+	
 	s->pc = top->pc;
 	s->snpc	= top->pc + 4;
 	s->dnpc	= top->rootp->ysyx_23060201_TOP__DOT__wire_dnpc;
@@ -164,9 +169,6 @@ static void exec_once(Decode *s) {
 	IFDEF(CONFIG_ITRACE, inst_trace(s));
 	IFDEF(CONFIG_FTRACE, func_trace(s));
 
-	top->inst = inst_fetch(&s->pc);
-	top->clk = 0; step_and_dump_wave();
-	top->clk = 1; step_and_dump_wave();
 	cpu_update();
 }
 
