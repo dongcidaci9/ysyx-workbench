18c975fd94d61da10826f6ff7227ad97c70951df
tracer-ysyx <tracer@ysyx.org>
2024-08-27 00:31:03 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:31:03 up  3:07,  3 users,  load average: 0.22, 0.43, 0.48

--- npc/csrc/utils/difftest.c
+++ npc/csrc/utils/difftest.c
@@ -60,7 +60,6 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
     ref_difftest_init(port);
     ref_difftest_memcpy(MBASE, guest_to_host(MBASE), img_size, DIFFTEST_TO_REF);
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-  printf("0x%08x\n", cpu.pc);
 }
 
 static bool difftest_checkregs(CPU_state *ref_r, addr_t pc) {
@@ -76,7 +75,6 @@ static bool difftest_checkregs(CPU_state *ref_r, addr_t pc) {
     }
     return true;
 }
-
 
 static void checkregs(CPU_state *ref, addr_t pc) {
   if (!difftest_checkregs(ref, pc)) {
