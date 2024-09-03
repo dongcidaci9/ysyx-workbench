c289417d545b436d387a535a3d5e9e6ec02217cc
tracer-ysyx <tracer@ysyx.org>
2024-09-03 23:12:52 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:12:52 up 14:15,  3 users,  load average: 1.06, 0.76, 0.75

--- npc/csrc/memory.c
+++ npc/csrc/memory.c
@@ -21,7 +21,7 @@ void init_isa() {
 // initialize memory
 void init_mem() {
 	mem = (uint8_t*)malloc(MSIZE);
-    printf("hello\n");
+    printf("0x%08x\n", *mem);
 }
 
 word_t host_read(void* addr) {
