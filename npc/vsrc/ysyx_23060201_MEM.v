e08d7334e9455d2547e0aea3973b56ef4ab1f1bb
tracer-ysyx <tracer@ysyx.org>
2024-09-03 23:33:47 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:33:47 up 14:36,  3 users,  load average: 1.72, 1.10, 0.96

--- npc/vsrc/ysyx_23060201_MEM.v
+++ npc/vsrc/ysyx_23060201_MEM.v
@@ -13,18 +13,15 @@ module ysyx_23060201_MEM(
   output reg [31:0]   mem_rdata 
 );
 
-  import "DPI-C" function void print(int mem_raddr); 
-  /* 
   import "DPI-C" function int pmem_read(input int mem_raddr);
+  /* 
   import "DPI-C" function void pmem_write(
     input int waddr, input int wdata, input byte wmask);
   */
 
   always @(*) begin
     if (mem_ren) begin
-      // mem_rdata = pmem_read(mem_raddr);
-      print(mem_raddr);
-      mem_rdata = 32'h0;
+      mem_rdata = pmem_read(mem_raddr);
     end
     else begin
       mem_rdata = 32'h0;
