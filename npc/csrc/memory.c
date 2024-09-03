fb26a6abf55fb880650b172bf7648eeaade2b7d1
tracer-ysyx <tracer@ysyx.org>
2024-09-03 22:49:10 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:49:10 up 13:51,  3 users,  load average: 1.42, 1.09, 0.94

--- npc/csrc/memory.c
+++ npc/csrc/memory.c
@@ -41,11 +41,11 @@ word_t inst_fetch(addr_t* pc_addr) {
 }
 
 extern "C" void print(addr_t mem_raddr) {
-    uint32_t memb;
+    uint32_t* memb = NULL;
 
     addr_t aligned_mem_raddr = mem_raddr & ~0x3u;
     
-    printf("0x%08x\n", memb);
+    printf("0x%08x\n", *memb);
 }
 
 extern "C" word_t pmem_read(addr_t raddr) {
