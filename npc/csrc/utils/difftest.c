bbf26585bddce77ab2ac89ca1a52faef50181176
tracer-ysyx <tracer@ysyx.org>
2024-09-04 22:14:26 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:26 up 20:14,  3 users,  load average: 0.58, 0.58, 0.50

--- npc/csrc/utils/difftest.c
+++ npc/csrc/utils/difftest.c
@@ -84,13 +84,13 @@ static void checkregs(CPU_state *ref, addr_t pc) {
   }
 }
 
-void difftest_step(addr_t pc) {
+void difftest_step(addr_t npc) {
   CPU_state ref_r;
 
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
-  checkregs(&ref_r, pc);
+  checkregs(&ref_r, npc);
 }
 
 #endif
