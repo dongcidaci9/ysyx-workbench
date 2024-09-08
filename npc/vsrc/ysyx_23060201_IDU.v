e2e57ef2a46ff9064d375e326477796cd39965c5
tracer-ysyx <tracer@ysyx.org>
2024-09-08 20:22:57 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:22:57 up 1 day, 16:46,  3 users,  load average: 0.48, 0.57, 0.52

--- npc/vsrc/ysyx_23060201_IDU.v
+++ npc/vsrc/ysyx_23060201_IDU.v
@@ -65,10 +65,11 @@ module ysyx_23060201_IDU(
 	// read R(rs1), R(rs2)
 
 	// gpr: ren[0] -> R(rs1), gpr_ren[1] -> R(rs2)
-	MuxKey #(8, 7, 2) gpr_ren_sel(gpr_ren, inst_op, {
+	MuxKeyWithDefault #(9, 7, 2) gpr_ren_sel(gpr_ren, inst_op, 2'b00, {
 		`ysyx_23060201_OP_TYPE_R,   2'b11,	
 		`ysyx_23060201_OP_TYPE_I,   2'b01,	
 		`ysyx_23060201_OP_TYPE_IL,  2'b11,	
+		`ysyx_23060201_OP_TYPE_S,   2'b11,	
 		`ysyx_23060201_OP_TYPE_B,   2'b11,	
 		`ysyx_23060201_OP_TYPE_U,   2'b00,	
 		`ysyx_23060201_OP_TYPE_UPC, 2'b00,	
