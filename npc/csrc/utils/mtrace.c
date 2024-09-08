20b5ecadc119de3a59f3dc793ebfa939e8cf2e9f
tracer-ysyx <tracer@ysyx.org>
2024-09-08 20:54:17 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:17 up 1 day, 17:16,  3 users,  load average: 0.91, 0.55, 0.49

--- npc/csrc/utils/mtrace.c
+++ npc/csrc/utils/mtrace.c
@@ -2,10 +2,10 @@
 
 #ifdef CONFIG_MTRACE
 void display_mread(addr_t addr, int len) {
-	printf("mem_read @" FMT_PADDR ", len = %d\n", addr, len);
+	printf("memory read @" FMT_PADDR ", len = %d\n", addr, len);
 }
 
 void display_mwrite(addr_t addr, int len, word_t data) {
-	printf("mem_write @" FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data);
+	printf("memory write @" FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data);
 }
 #endif
