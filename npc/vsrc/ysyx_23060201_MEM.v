37d122edfce27bd863b347a528eb0f99852fbccf
tracer-ysyx <tracer@ysyx.org>
2024-09-08 14:17:12 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:12 up 1 day, 10:47,  3 users,  load average: 0.50, 0.37, 0.44

--- npc/vsrc/ysyx_23060201_MEM.v
+++ npc/vsrc/ysyx_23060201_MEM.v
@@ -33,7 +33,7 @@ module ysyx_23060201_MEM # (
       pmem_write(mem_waddr, mem_wdata, mem_wmask);
     end 
     else begin
-      pmem_write(mem_waddr, mem_wdata, mem_wmask);
+      pmem_write(mem_waddr, mem_wdata, 8'b0000);
     end
   end
 
