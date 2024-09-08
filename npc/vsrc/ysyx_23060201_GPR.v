23f9643b1d932cc3bf02df9b5b81500fb06cc4b1
tracer-ysyx <tracer@ysyx.org>
2024-09-08 20:29:25 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:25 up 1 day, 16:52,  3 users,  load average: 0.72, 0.71, 0.60

--- npc/vsrc/ysyx_23060201_GPR.v
+++ npc/vsrc/ysyx_23060201_GPR.v
@@ -21,7 +21,7 @@ module ysyx_23060201_GPR # (
 	end
 	
 	// Write back the changed value
-	always @(posedge clk) begin
+	always @(negedge clk) begin
     	if (gpr_wen) reg_file[gpr_waddr] <= (gpr_waddr != 5'd0) ? gpr_wdata : 32'b0;
   	end
 
