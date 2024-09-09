c95fe5e9d8f9f0d0caaad9e15a05eca0068bc49a
tracer-ysyx <tracer@ysyx.org>
2024-09-09 11:39:40 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:39:40 up 2 days,  7:58,  3 users,  load average: 0.75, 0.58, 0.51

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -218,8 +218,7 @@ int main(int argc, char *argv[]) {
 	
 	init_monitor(argc, argv);
 
-	top->clk = 0; step_and_dump_wave();
-	top->clk = 1; step_and_dump_wave();
+	top->rst_n = 1;
 	cpu_update();
 	
 	sdb_mainloop();
