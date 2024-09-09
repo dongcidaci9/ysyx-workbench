3b84e07ee560e6e249b660433e5bc84750125dbc
tracer-ysyx <tracer@ysyx.org>
2024-09-09 12:39:07 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:39:07 up 2 days,  8:58,  3 users,  load average: 0.55, 0.48, 0.45

--- npc/vsrc/ysyx_23060201_MEM.v
+++ npc/vsrc/ysyx_23060201_MEM.v
@@ -21,7 +21,7 @@ module ysyx_23060201_MEM # (
   import "DPI-C" function void pmem_write(
     input int mem_waddr, input int mem_wdata, input byte mem_wmask);
 
-  always @(posedge clk) begin
+  always @(negedge clk) begin
     if (mem_ren) begin
       mem_rdata <= pmem_read(mem_raddr, mem_rmask);
     end
