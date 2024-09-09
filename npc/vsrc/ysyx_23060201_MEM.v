2c80f3e1f48059be6395d21ecc08a58e7e9bdfdf
tracer-ysyx <tracer@ysyx.org>
2024-09-09 23:42:04 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:42:04 up 2 days, 13:36,  3 users,  load average: 0.55, 0.45, 0.30

--- npc/vsrc/ysyx_23060201_MEM.v
+++ npc/vsrc/ysyx_23060201_MEM.v
@@ -22,16 +22,15 @@ module ysyx_23060201_MEM # (
     input int mem_waddr, input int mem_wdata, input byte mem_wmask);
 
   always @(*) begin
-    $display("%h", mem_raddr);
+    if (mem_ren) begin
+      mem_rdata = pmem_read(mem_raddr, mem_rmask);
+    end
+    else begin
+      mem_rdata = 32'h0; 
+    end
   end
 
   always @(posedge clk) begin
-    if (mem_ren) begin
-      mem_rdata <= pmem_read(mem_raddr, mem_rmask);
-    end
-    else begin
-      mem_rdata <= 32'h0; 
-    end
     if (mem_wen) begin
       pmem_write(mem_waddr, mem_wdata, mem_wmask);
     end 
