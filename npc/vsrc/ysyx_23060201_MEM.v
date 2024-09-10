d67f5b578d2ec34205cee2ffb855cd274d998528
tracer-ysyx <tracer@ysyx.org>
2024-09-10 17:33:09 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:33:09 up  4:44,  3 users,  load average: 1.12, 0.62, 0.55

--- npc/vsrc/ysyx_23060201_MEM.v
+++ npc/vsrc/ysyx_23060201_MEM.v
@@ -6,7 +6,7 @@ module ysyx_23060201_MEM # (
 ) 
 (
   input wire                        clk                 ,
-  input wire                        rst_n               ,
+  // input wire                        rst_n               ,
   input wire                        mem_wen             , 
   input wire [MEM_ADDR_WIDTH-1:0]   mem_waddr           , 
   input wire [7:0]                  mem_wmask           ,           
@@ -17,15 +17,17 @@ module ysyx_23060201_MEM # (
   output reg [DATA_WIDTH-1:0]       mem_rdata 
 );
 
+  /*
   reg                               mem_rstop           ;
 
   Reg #(1, 'b0) mem_rstop_reg(
-    clk, ~rst_n, ~mem_rstop, mem_rstop, 1'b1 
+    ~clk, ~rst_n, ~mem_rstop, mem_rstop, 1'b1 
   );
 
   initial begin
     mem_rstop = 'b0;
   end
+  */
 
   import "DPI-C" function int pmem_read(
     input int mem_raddr, input byte mem_rmask);
@@ -33,7 +35,7 @@ module ysyx_23060201_MEM # (
     input int mem_waddr, input int mem_wdata, input byte mem_wmask);
 
   always @(*) begin
-    if (mem_ren && ~mem_rstop) begin
+    if (mem_ren && ~clk) begin
       mem_rdata = pmem_read(mem_raddr, mem_rmask);
     end
     else begin
