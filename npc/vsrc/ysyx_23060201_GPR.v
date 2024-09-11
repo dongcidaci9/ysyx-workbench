d1a1fed8bd63e9e429e4f0a0ceafe16c24c8b16e
tracer-ysyx <tracer@ysyx.org>
2024-09-11 19:52:46 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:52:46 up 49 min,  3 users,  load average: 0.42, 0.42, 0.36

--- npc/vsrc/ysyx_23060201_GPR.v
+++ npc/vsrc/ysyx_23060201_GPR.v
@@ -4,7 +4,7 @@ module ysyx_23060201_GPR # (
 ) 
 (
 	input wire							clk					, 
-	input wire							rst_n				, 
+	// input wire							rst_n				, 
 	input wire [1:0] 					gpr_ren				, 
 	input wire 							gpr_wen				, 
 	input wire 	[GPR_ADDR_WIDTH-1:0] 	gpr_waddr			, 
@@ -16,29 +16,24 @@ module ysyx_23060201_GPR # (
 );
 
 	reg [DATA_WIDTH-1:0] 	reg_file [2**GPR_ADDR_WIDTH-1:0]	; 
-	reg						valid								;
+	
 	integer i;
 	initial begin
-		valid = 'b1;
 		for (i = 0; i < 32; i = i + 1) begin
 			reg_file[i] = 'h0;
 		end
 	end
 	
 	// read gpr 
-	MuxKeyWithDefault #(2, 1, 32) gpr_rdata1_sel(gpr_rdata1, valid && gpr_ren[0], 32'b0, {
+	MuxKeyWithDefault #(2, 1, 32) gpr_rdata1_sel(gpr_rdata1, ~clk && gpr_ren[0], 32'b0, {
 		1'b0, 	32'b0,	
 		1'b1,	reg_file[gpr_raddr1]	
 	});
 
-	MuxKeyWithDefault #(2, 1, 32) gpr_rdata2_sel(gpr_rdata2, valid && gpr_ren[1], 32'b0, {
+	MuxKeyWithDefault #(2, 1, 32) gpr_rdata2_sel(gpr_rdata2, ~clk && gpr_ren[1], 32'b0, {
 		1'b0, 	32'b0,	
 		1'b1,	reg_file[gpr_raddr2]	
 	});
-
-	Reg #(1, 'b1) valid_reg(
-		clk, ~rst_n, 1'b1, valid, 1'b1
-	);
 
 	// write gpr
 	always @(posedge clk) begin
