0ec1071c004efaf2fd69c15ff7d7ad9b3adbbd9b
tracer-ysyx <tracer@ysyx.org>
2024-09-13 11:15:24 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:24 up  8:37,  3 users,  load average: 0.36, 0.36, 0.32

--- npc/csrc/memory.c
+++ npc/csrc/memory.c
@@ -76,14 +76,14 @@ extern "C" word_t pmem_read(addr_t raddr, char rmask) {
 }
 
 extern "C" void pmem_write(addr_t waddr, word_t wdata, char wmask) {
-  // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
-  // `wmask`中每比特表示`wdata`中1个字节的掩码,
-  // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-    addr_t aligned_waddr = waddr & ~0x3u;
+    // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
+    // `wmask`中每比特表示`wdata`中1个字节的掩码,
+    // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
+    // addr_t aligned_waddr = waddr & ~0x3u;
     char len = 0;
     for (int i = 0; i < 4; i ++) {
         if (wmask & (1 << i)) {
-            memset(guest_to_host(aligned_waddr) + i, (wdata >> (i * 8)) & 0xFF, 1);
+            memset(guest_to_host(waddr) + i, (wdata >> (i * 8)) & 0xFF, 1);
             len ++; 
         }
     }
