1cc8671e8f0f1bab01535659776ab4ee9fa4d990
tracer-ysyx <tracer@ysyx.org>
2024-09-13 15:33:06 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:06 up 12:02,  3 users,  load average: 1.60, 0.83, 0.59

--- npc/csrc/memory.c
+++ npc/csrc/memory.c
@@ -34,18 +34,13 @@ word_t mem_read(addr_t addr) {
 
 	return ret;
 }
-/*
-// addr_t* pc_addr = &pc; *pc_addr = pc; 
-word_t inst_fetch(addr_t* pc_addr) {
-	uint32_t inst = mem_read(*pc_addr);
-	return inst;
-}
-*/
+
 extern "C" int pmem_read(addr_t raddr, char rmask) {
 	// 总是读取地址为`raddr & ~0x3u`的4字节返回
     // addr_t aligned_raddr = raddr & ~0x3u;
     word_t rdata = host_read(guest_to_host(raddr));
-
+    if (raddr == 时钟地址) { 返回当前时间 }
+    
     char rmask1 = rmask & 0x0F;
     char rmask2 = (rmask & 0x10) >> 4;
 
@@ -82,7 +77,9 @@ extern "C" void pmem_write(addr_t waddr, word_t wdata, char wmask) {
     // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
     // `wmask`中每比特表示`wdata`中1个字节的掩码,
     // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-    // addr_t aligned_waddr = waddr & ~0x3u;
+    // addr_t aligned_waddr = waddr & ~0x3u; 
+    if (waddr == 串口地址) { putchar(...) }
+    
     char len = 0;
     for (int i = 0; i < 4; i ++) {
         if (wmask & (1 << i)) {
