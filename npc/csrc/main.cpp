1c5d1d91b3e4bd6c2c141435d54dd5bc49fd4b44
tracer-ysyx <tracer@ysyx.org>
2024-09-13 21:12:38 +0800

    > sim RTL ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:38 up 31 min,  1 user,  load average: 1.50, 0.66, 0.36

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -170,8 +170,14 @@ static void func_trace(Decode *s)
 #endif
 
 static void exec_once(Decode *s) {
-	top->clk = 1; step_and_dump_wave();
-	top->clk = 0; step_and_dump_wave();
+	if (g_nr_guest_inst == 0) {
+		top->rst_n = 0;
+		top->clk = 0; step_and_dump_wave();
+		top->rst_n = 1;
+	} else {
+		top->clk = 1; step_and_dump_wave();
+		top->clk = 0; step_and_dump_wave();
+	}
 	
 	s->pc	= top->pc;
 	s->snpc	= top->pc + 4;
@@ -229,9 +235,6 @@ int main(int argc, char *argv[]) {
 	
 	init_monitor(argc, argv);
 
-	top->rst_n = 0;
-	top->clk = 0; step_and_dump_wave();
-	top->rst_n = 1;
 	cpu_update();
 	
 	sdb_mainloop();
