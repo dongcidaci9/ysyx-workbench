6f4ad0d86e42a4933263d6532e9673b11681e8ba
tracer-ysyx <tracer@ysyx.org>
2024-09-14 18:43:21 +0800

    > compile NEMU ysyx_2023 LZQ Linux dcdc9 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:21 up  1:41,  3 users,  load average: 1.23, 0.93, 0.66

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,10 +52,13 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  word_t ret = pmem_read(addr, len);
-	IFDEF(CONFIG_MTRACE, display_pread(addr, len, ret));
-  if (likely(in_pmem(addr))) return ret;
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  word_t ret;
+  if (likely(in_pmem(addr))) ret = pmem_read(addr, len);
+  IFDEF(CONFIG_DEVICE, 
+    ret = mmio_read(addr, len));
+
+  return ret;
+  IFDEF(CONFIG_MTRACE, display_pread(addr, len, ret));
   out_of_bound(addr);
   return 0;
 }
